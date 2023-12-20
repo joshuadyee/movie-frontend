3804d685373e617456bb711f3f54dea248005bdf
Joshua Yee <joshyee16@gmail.com>
2023-12-19 22:06:40 -0800

    init

---
+++ src/Content.jsx
@@ -0,0 +1,7 @@
+export function Content() {
+  return (
+    <div>
+      <h1>Welcome to my Movies Page!</h1>
+    </div>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <a href="#">Home</a> | <a href="#movies-index">All Movies</a> | <a href="#movies-new">New Entry</a>
+    </header>
+  )
+}
